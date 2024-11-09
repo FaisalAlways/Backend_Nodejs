797314b0aa03208a9192fc9cdcd1a898674676c7
faisal <faisalhossain7474@gmail.com>
2024-11-09 12:41:28 +0600

    crud

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const userModel = require('./usermodel');
+
+app.get('/', (req, res) =>{
+    res.send('Faisal');
+});
+
+app.get('/create', async (req, res) => {
+    let createdUser = await userModel.create({
+        name: "Faisal",
+        email: "faisalhossain7474@gmail.com",
+        username: "faisal"
+    });
+    res.send([createdUser]);
+});
+
+app.get('/update', async (req, res) => {
+    let updateduser = await userModel.findOneAndUpdate({username: "faisal"}, {name: "Faisal Hossain"}, {new: true}); 
+   res.send(updateduser);
+});
+
+app.get('/read', async (req, res) => {
+  let users = await userModel.find()
+  res.send(users);
+});
+
+app.get('/delete', async (req, res) => {
+  let users = await userModel.findOneAndDelete()
+  res.send(users);
+});
+
+app.listen(5000, function(){
+    console.log("server is still running on port 000");
+    
+});
